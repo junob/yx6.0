b634a95a3dec9d4aed110d66d6efa9a11ee1d455
junob <805517373@qq.com>
2016-07-23 10:09:12 +0800

    yx6.0

---
+++ js/wy.js
@@ -0,0 +1,16 @@
+
+$(function(){
+    $("#fullpage").fullpage({
+        navigation:true,
+        anchors:['page1','page2','page3','page4','page5','page6','page7','page8','page9','page10'],
+        navigationTooltips:['首页','视觉','交互','皮肤','功能','待办邮件','联系人邮件','科技','连接易信','马上体验'],
+        afterLoad:function(){
+            $(this).addClass("actives");
+        },
+        onLeave:function(){
+            $(this).removeClass("actives");
+        }
+
+    })
+
+})
